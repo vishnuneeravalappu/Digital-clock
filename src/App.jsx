1d9285628e9dd51c75522a3975640e7f5143d591
VishnuSharma K (WIPRO LIMITED) <v-vishnusk@microsoft.com>
2023-11-16 10:05:22 +0530

    performance improvements

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,25 @@ import { TimeCard } from "./components/components-time-box/time-card";
 import {TimeSeperator, Meridium} from './components/components-time-box/time-card-accessories';
 
 const App = () =>{
+    let time = new Date;
     const [dateTime, setDateTime] = React.useState({
                 hours: "",
                 minutes: "",
                 seconds: "",
             });
     React.useEffect(()=>{
-        setInterval(()=>{
+        setTimeout(()=>{
             let time = new Date;
             let hours = time.getHours();
             let minutes = time.getMinutes();
             let seconds = time.getSeconds();
             setDateTime({
-                hour: hours>=10 ? hours.toString() : hours.toString().padStart(2, "0"),
-                minute: minutes>=10 ? minutes.toString() : minutes.toString().padStart(2, "0"),
-                second: seconds>=10 ? seconds.toString() : seconds.toString().padStart(2, "0"),
+                hour: hours.toString().padStart(2, "0"),
+                minute: minutes.toString().padStart(2, "0"),
+                second: seconds.toString().padStart(2, "0"),
             })
-        }, 1000)
-    },[])
+        }, 500)
+    },[time])
     return (
         <div className="clock-container">
             <TimeCard time={dateTime.hour}/>
